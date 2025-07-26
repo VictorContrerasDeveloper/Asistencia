b721c77a1bdbad60a54eb974b1c87ad449dd4b62
Victor <victorandres.contacto@gmail.com>
2025-07-26 00:16:57 +0000

    figura este error.. que signidica?

--- src/components/ui/alert-dialog.tsx
+++ src/components/ui/alert-dialog.tsx
@@ -42,7 +42,7 @@ const AlertDialogContent = React.forwardRef<
       )}
       {...props}
     />
-   </AlertDialogPortal>
+  </AlertDialogPortal>
 ))
 AlertDialogContent.displayName = AlertDialogPrimitive.Content.displayName
 
